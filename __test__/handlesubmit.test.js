bed8f2e5ffbae644fbff2f80f7e86ca2c2ce0884
BrickBrigade <101427661+BrickBrigade@users.noreply.github.com>
2022-06-29 01:05:26 -0500

    Added service worker and a test for handleSubmit.

---
+++ __test__/handlesubmit.test.js
@@ -0,0 +1,7 @@
+import { handleSubmit } from "../src/client/js/formHandler";
+
+describe("Testing the submit function", () => {
+	test("Testing handleSubmit() function", () => {
+		expect(handleSubmit).toBeDefined();
+	})
+})
